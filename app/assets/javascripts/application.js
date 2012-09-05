0980407f6616c8a128bc7dc32b33bf15b21bf738
Garrett Heinlen <heinleng@gmail.com>
2012-09-05 09:38:34 -0500

    adding fade effect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,26 +18,26 @@
 $(function() {
   $('nav a').pjax('[data-pjax-container]')
 
-  $('[data-pjax-container]').bind("pjax:beforeSend.pjax", function() {
-    // return $('#page').hide(0);
-    $('#page').animate({
-      height: 0,
-      minHeight: 0,
-      opacity: 0
-    }, 500);
-  }).bind("end.pjax", function() {
-    $('#page').animate({
-      height: 590,
-      minHeight: 590,
-      opacity: 1
-    }, 900);
-  });
-
-  // $('[data-pjax-container]').bind("start.pjax", function() {
-  //   return $('#page').hide(0);
+  // $('[data-pjax-container]').bind("pjax:beforeSend.pjax", function() {
+  //   // return $('#page').hide(0);
+  //   $('#page').animate({
+  //     height: 0,
+  //     minHeight: 0,
+  //     opacity: 0
+  //   }, 500);
   // }).bind("end.pjax", function() {
-  //   return $('#page').fadeIn(500);
+  //   $('#page').animate({
+  //     height: 590,
+  //     minHeight: 590,
+  //     opacity: 1
+  //   }, 900);
   // });
+
+  $('[data-pjax-container]').bind("start.pjax", function() {
+    return $('#page').hide(0);
+  }).bind("end.pjax", function() {
+    return $('#page').fadeIn(1000);
+  });
 
 
 });
