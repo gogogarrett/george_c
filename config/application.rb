4cc87c4c91675d3e815804e18b79f749c7210c71
Garrett Heinlen <heinleng@gmail.com>
2012-09-09 15:11:19 -0500

    asset compiling

--- config/application.rb
+++ config/application.rb
@@ -61,6 +61,6 @@ module GeorgeC
     config.assets.version = '1.0'
 
     # needed for refinery
-    config.assets.initialize_on_precompile = true
+    config.assets.initialize_on_precompile = false
   end
 end
