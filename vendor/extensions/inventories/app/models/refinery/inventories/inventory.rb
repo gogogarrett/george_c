4e3fcbe16547c6c86f482736fc764a01138f69b9
Garrett Heinlen <heinleng@gmail.com>
2012-10-03 23:12:56 -0500

    fixed contact page and fixed small css issues

--- vendor/extensions/inventories/app/models/refinery/inventories/inventory.rb
+++ vendor/extensions/inventories/app/models/refinery/inventories/inventory.rb
@@ -3,7 +3,7 @@ module Refinery
     class Inventory < Refinery::Core::BaseModel
       self.table_name = 'refinery_inventories'
 
-      attr_accessible :title, :description, :details, :price, :address, :image_id, :position
+      attr_accessible :title, :description, :details, :price, :address, :image_id, :positionm
 
       acts_as_indexed :fields => [:title, :description, :details, :price, :address]
 
